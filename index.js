8584c08c9c85864680e7b310a5e2e12a1f44a996
Quentin Lourme <quentin.lourme@3wa.io>
2024-03-04 11:39:22 +0100

    fs

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
-import chalk from "chalk";
+import fs from "fs";
+import http from "http";
+import axios from "axios";
 
-const students = ["Marianne", "Jason", "Nathan", "Sofiane"];
-const colors = [chalk.red, chalk.green, chalk.blue, chalk.yellow];
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-students.forEach((student, index) => {
-  const color = colors[index % colors.length];
-  console.log(color(student));
+fs.writeFile("test.txt", "Hello world!", (err) => {
+  if (err) throw err;
+  console.log("File has been saved");
+
+  fs.readFile("test.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log({ data });
+  });
 });
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello, world!</h1>");
+  res.end();
+});
+
+server.listen(8000);
+
+// const server = http.createServer((req, res) => {
+//   fs.readFile(path.join(__dirname, 'google.html'), (err, data) => {
+//     if (err) {
+//       res.writeHead(404);
+//       res.end(JSON.stringify(err));
+//       return;
+//     }
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end(data);
+//   });
+// });
+
+// server.listen(8000);
+
+axios
+  .get("http://www.google.com")
+  .then((response) => {
+    fs.writeFile("google.html", response.data, (err) => {
+      if (err) throw err;
+      console.log("Google file has been saved");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
